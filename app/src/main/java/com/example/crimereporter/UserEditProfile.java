516e7be4e744d4453c94122738286bae54cca36c
hetsh <hetshreejuthani@gmail.com>
2022-03-29 10:17:54 +0530

    policemenu

--- app/src/main/java/com/example/crimereporter/UserEditProfile.java
+++ app/src/main/java/com/example/crimereporter/UserEditProfile.java
@@ -4,17 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import java.util.Calendar;
-import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class UserEditProfile extends AppCompatActivity {
 
