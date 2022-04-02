b5fa6f085728270470609ee2cd84cd704b5a064d
hetsh <hetshreejuthani@gmail.com>
2022-04-02 11:14:48 +0530

    h

--- app/src/main/java/com/example/crimereporter/UserViewProfile.java
+++ app/src/main/java/com/example/crimereporter/UserViewProfile.java
@@ -2,24 +2,13 @@ package com.example.crimereporter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class UserViewProfile extends AppCompatActivity {
-    Button updatebtnup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_view_profile);
-        updatebtnup = findViewById(R.id.upupdatebtn);
-        updatebtnup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), UserEditProfile.class);
-                startActivity(i);
-            }
-        });
     }
 }
